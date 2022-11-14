6bbdbd0e49f17e0afb7eacc80fa69818c30f20b6
LNMIG <migliorenicolas@yahoo.com.ar>
2022-11-14 18:34:38 -0300

    DOCS: files for finding products on the db

---
+++ pages/api/avo/[id].js
@@ -0,0 +1,11 @@
+import DB from "@database"
+
+const AvoDetail = async (req, res) => {
+    const db = new DB()
+    const avoId = req.query.id
+    const oneEntry = await db.getById(avoId)
+
+    res.status(200).json(oneEntry)
+}
+
+export default AvoDetail
