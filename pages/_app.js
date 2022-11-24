769e98f39f66b9b978175e949c334075e6193757
LNMIG <migliorenicolas@yahoo.com.ar>
2022-11-24 09:42:42 -0300

    Minor change

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,10 @@ import useInitialState from '@hooks'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
+export function reportWebVitals(metric) {
+  console.log(metric)
+}
+
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState()
   return (
